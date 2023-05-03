323214b7ecfce49a76b6360c1bcd2eaf49d1df37
Komal-Bhurse <komalbhurse3@gmail.com>
2023-05-03 23:54:32 +0530

    16th commit

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,6 +1,6 @@
 function cors(){
     return (req,res,next)=>{
-        res.setHeader('Access-Control-Allow-Origin','*');
+        res.setHeader('Access-Control-Allow-Origin','https://cakeat-ecom.vercel.app');
         res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
         res.setHeader('Access-Control-Allow-Headers','Content-Type');
         res.setHeader('Access-Control-Allow-Credentials',true);
