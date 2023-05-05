9a38403a6f31fa195e63948295355a9976513a3d
Komal-Bhurse <komalbhurse3@gmail.com>
2023-05-05 14:30:28 +0530

    testing

--- client/src/components/CakeItem.js
+++ client/src/components/CakeItem.js
@@ -51,7 +51,7 @@ function CakeItem() {
             key={product._id}
           >
             <img
-              src={`https://cakeat.vercel.app/${product.Image}`}
+              src={`${product.Image}`}
               alt={product.Name}
               className="w-44 h-36 md:h-40 rounded-lg mx-auto cursor-pointer"
               onClick={() => navigate(`/product/${product._id}`)}
